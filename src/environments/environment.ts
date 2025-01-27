0764ecad197da4c8360d50e54c5a5dc23cce63db
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-01-27 17:50:19 +0500

    language

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   clientId: '2803466287-fq6t97fbmls43nvu29nkhhtioh6tfemv.apps.googleusercontent.com',
-  redirectUri: location.origin + '/oauth-callback',
+  redirectUri: 'https://dynamics-market.netlify.app' + '/oauth-callback',
   authUrlParams: 'response_type=id_token&scope=email%20profile%20openid&nonce=123456789',
   host: 'http://95.130.227.52:5000/',
   //host: 'http://192.168.131.18:5000/',
