95b899015d23f6079930e42c5c1df6d28e6bf602
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-02-02 18:20:48 +0500

    commit

--- functions/server.js
+++
@@ -1,8 +0,0 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
-
-// Your SSR logic here
-app.use(express.static('./dist/stroyka/browser'));
-
-exports.handler = serverless(app);
--- server.ts
+++ server.ts
@@ -7,17 +7,16 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
-import { Express } from 'express';
 
-export function app(): Express {
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/stroyka/browser');
+  const distFolder = join(process.cwd(), 'dist/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
     bootstrap: AppServerModule,
-    inlineCriticalCss: false,
   }));
 
   server.set('view engine', 'html');
