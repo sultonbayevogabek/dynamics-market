7951409a8b568e5ea74987bbd140627c00678035
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-02-03 18:04:19 +0500

    language

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { environment } from '@env/environment';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { IUser } from '../interfaces/user';
 import { TranslateService } from '@ngx-translate/core';
 import { ToasterService } from './toaster.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,7 @@ export class AuthService {
     private toaster: ToasterService,
     private http: HttpClient,
     private translateService: TranslateService,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {
     // @ts-ignore
     window.receiveData = this.receiveData.bind(this);
@@ -25,11 +27,16 @@ export class AuthService {
 
   set token(token: string) {
     if (!token) return;
-    localStorage.setItem('token', token);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('token', token);
+    }
   }
 
   get token(): string {
-    return localStorage.getItem('token') ?? '';
+    if (isPlatformBrowser(this.platformId)) {
+      return localStorage.getItem('token') ?? '';
+    }
+    return '';
   }
 
   set authorized(auth: boolean) {
@@ -41,30 +48,32 @@ export class AuthService {
   }
 
   openOAuthWindow() {
-    const screenWidth = window.screen.width;
-    const screenHeight = window.screen.height;
+    if (isPlatformBrowser(this.platformId)) {
+      const screenWidth = window.screen.width;
+      const screenHeight = window.screen.height;
 
-    let windowWidth: number, windowHeight: number;
+      let windowWidth: number, windowHeight: number;
 
-    if (screenWidth <= 768) {
-      windowWidth = screenWidth;
-      windowHeight = screenHeight;
-    } else if (screenWidth <= 1024) {
-      windowWidth = screenWidth * 0.8;
-      windowHeight = screenHeight * 0.8;
-    } else {
-      windowWidth = screenWidth * 0.6;
-      windowHeight = screenHeight * 0.6;
+      if (screenWidth <= 768) {
+        windowWidth = screenWidth;
+        windowHeight = screenHeight;
+      } else if (screenWidth <= 1024) {
+        windowWidth = screenWidth * 0.8;
+        windowHeight = screenHeight * 0.8;
+      } else {
+        windowWidth = screenWidth * 0.6;
+        windowHeight = screenHeight * 0.6;
+      }
+
+      const left = (screenWidth - windowWidth) / 2;
+      const top = (screenHeight - windowHeight) / 2;
+
+      const oauthWindow = window.open(
+        `https://accounts.google.com/o/oauth2/v2/auth?client_id=${ environment.clientId }&redirect_uri=${ environment.redirectUri }&${ environment.authUrlParams }`,
+        '_blank',
+        `width=${ windowWidth },height=${ windowHeight },top=${ top },left=${ left }`
+      );
     }
-
-    const left = (screenWidth - windowWidth) / 2;
-    const top = (screenHeight - windowHeight) / 2;
-
-    const oauthWindow = window.open(
-      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${ environment.clientId }&redirect_uri=${ environment.redirectUri }&${ environment.authUrlParams }`,
-      '_blank',
-      `width=${ windowWidth },height=${ windowHeight },top=${ top },left=${ left }`
-    );
   }
 
   authWithGoogle(idToken: string): Observable<{ token: string }> {
