852f0ab5ac2c14bc2ee074ce0bcb16ca1d93e7c2
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-02-15 23:01:34 +0500

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   googleAuthParams: {
-    clientId: '2803466287-fq6t97fbmls43nvu29nkhhtioh6tfemv.apps.googleusercontent.com',
-    redirectUri: window.location.origin + '/oauth-callback',
+    client_id: '2803466287-fq6t97fbmls43nvu29nkhhtioh6tfemv.apps.googleusercontent.com',
+    redirect_uri: window.location.origin + '/oauth-callback',
     response_type: 'id_token',
     scope: 'email profile openid',
     nonce: '123456789'
