ead0d03109156b5b98cf123976417a633c817e70
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-04-22 23:42:04 +0500

    commit

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -22,9 +22,9 @@ import { environment } from '@env/environment.prod';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: [ './product-card.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class ProductCardComponent implements OnInit, OnDestroy, OnChanges {
   host = environment.host;
   private destroy$: Subject<void> = new Subject();
