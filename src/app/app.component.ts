d966383c90d35aab1bdef27ace90fba4f719a255
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-05-20 16:01:21 +0500

    Refactor preloader logic and design.
    Replaced the existing preloader implementation with a more efficient and visually updated version. Transition styles and animation were streamlined, and redundant code was removed to improve maintainability and cross-browser compatibility. Updated the preloader structure and integrated a new SVG loader animation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,29 +23,6 @@ export class AppComponent implements OnInit {
   ) {
     if (isPlatformBrowser(this.platformId)) {
       this.zone.runOutsideAngular(() => {
-        this.router.events.pipe(filter(event => event instanceof NavigationEnd), first()).subscribe(() => {
-          const preloader = document.querySelector('.site-preloader');
-
-          if (preloader === null) {
-            return;
-          }
-
-          preloader.addEventListener('transitionend', (event: Event) => {
-            if (event instanceof TransitionEvent && event.propertyName === 'opacity') {
-              preloader.remove();
-            }
-          });
-          preloader.classList.add('site-preloader__fade');
-
-          // Sometimes, due to unexpected behavior, the browser (in particular Safari) may not play the transition, which leads
-          // to blocking interaction with page elements due to the fact that the preloader is not deleted.
-          // The following block covers this case.
-          if (getComputedStyle(preloader).opacity === '0' && preloader.parentNode) {
-            preloader.parentNode.removeChild(preloader);
-          }
-        });
-
-
         // Language init
         const lang = localStorage.getItem('lang') || 'uz';
         if (LANGUAGES_SHORTS.includes(lang)) {
